19ac5c493ea01fa36777cfbe23b91db2972c6284
Juliano Gomes <js.gomes1@hotmail.com>
2019-05-31 00:53:51 -0300

    add js

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,29 @@
+// var connection 
+// document.onreadystatechange = () => {
+//     if (document.readyState === 'complete') {
+//         var conn = document.getElementById('connected')
+//         var disconn = document.getElementById('disconnected')
+//     }
+//   };
 
-var connection = new WebSocket('ws://192.168.4.1:8083');
+var disconn
+var conn
+var tecla
+
+window.onload=function(){
+    disconn = document.getElementById('disconnected')
+    conn = document.getElementById('connected')
+    tecla =  document.getElementById('tec')
+}   
+
+connection = new WebSocket('ws://127.0.0.1:8083');
+
 
 connection.onopen = function () {
+    disconn.style.display = 'none';
+    // disconn.innerHTML = "teste"
+    conn.style.display = ""
+
     var pressed = document.getElementById('pressed');
 };
 
@@ -9,25 +31,35 @@ connection.onerror = function (error) {
 console.log('WebSocket Error ' + error);
 };
 
-// connection.onmessage = function (e) {
-// console.log('Server: ' + e.data);
-// };
+connection.onclose = function (a){
+    disconn.style.display = ''
+    disconn.innerHTML += a 
+    conn.style.display = 'none'
+    // setTimeout(connect, 1000)
+};
 
 function keyPressed(evt){
-        evt = evt || window.event;
-        var key = evt.keyCode || evt.which;
-        return String.fromCharCode(key); 
-    };
-    var pressionada = [];
-    document.onkeydown = function(evt) {
-        if (!pressionada[evt.keyCode - 96]){
-            var str = keyPressed(evt);
-            connection.send((evt.keyCode - 96)+'d'); 
-            pressionada[evt.keyCode - 96] = true;
-        }
-    };
-    document.onkeyup = function(evt) {
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+};
+var pressionada = [];
+document.onkeydown = function(evt) {
+    if (!pressionada[evt.keyCode - 96]){
         var str = keyPressed(evt);
-        pressionada[evt.keyCode - 96] = false;
-        connection.send((evt.keyCode - 96)+'u');
-    };
+        connection.send((evt.keyCode - 96)+'d'); 
+        pressionada[evt.keyCode - 96] = true;
+        tecla.innerText =  str
+    }
+};
+document.onkeyup = function(evt) {
+    var str = keyPressed(evt);
+    pressionada[evt.keyCode - 96] = false;
+    connection.send((evt.keyCode - 96)+'u');
+};
+
+
+
+
+// function connect(){
+
